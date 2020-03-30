2b89619a56029ebf627578e6c2b3985d1cc9e713
James Ronan <james@ronanweb.co.uk>
2020-03-30 11:08:13 +0100

    Make the search method actually work.
    GET requests need URL parameters, James. How long have you worked with the
    Internet?

--- lib/SMS/MessageBird/API.pm
+++ lib/SMS/MessageBird/API.pm
@@ -5,6 +5,7 @@ use warnings;
 
 use LWP::UserAgent;
 use JSON;
+use URI;
 
 =head1 NAME
 
@@ -142,10 +143,21 @@ sub _api_request {
 
     my %request_params;
     if ($data) {
-        my $content_payload = JSON->new->encode($data);
 
-        $request_params{'Content-Type'} = 'application/json',
-        $request_params{Content} = $content_payload;
+        # For a GET request, we need to send the $data as request params.
+        if (lc $method eq 'get') {
+            my $get_url = URI->new($endpoint);
+            $get_url->query_form(%$data);
+            $endpoint = $get_url;
+
+        # For the others POST, PUT, PATCH - send the data as payload.
+        } else {
+
+            my $content_payload = JSON->new->encode($data);
+
+            $request_params{'Content-Type'} = 'application/json',
+            $request_params{Content} = $content_payload;
+        }
     }
 
     my $api_response = $self->{ua}->$method(
